687fcca2080b4d20dd642fdab302ab038b9481c7
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2025-02-22 19:11:17 +0900

    [level 2] Title: 주식가격, Time: 70.29 ms, Memory: 19.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    
+    queue = deque(prices)
+    
+    while queue:
+        price = queue.popleft()
+        count = 0
+        
+        for q in queue:
+            count += 1
+            if price > q:
+                break
+        # print("queue: ", queue)
+        answer.append(count)
+    
+    
+    return answer
