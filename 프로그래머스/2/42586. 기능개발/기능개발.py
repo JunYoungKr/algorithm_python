4b7ad09fd5f970264456c4c0d7726cfb4ebaa093
Jun Young Kim <97329075+JunYoungKr@users.noreply.github.com>
2025-02-23 16:12:42 +0900

    [level 2] Title: 기능개발, Time: 0.44 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,25 @@
 import math
-from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    
-    stack = deque()
+    time = []
+    stack = []
     
     for i in range(len(progresses)):
-        stack.append(math.ceil((100 - progresses[i]) / speeds[i]))
+        time.append(math.ceil((100 - progresses[i]) / speeds[i]))
     
-    #
-    while stack:
-        # 첫 번째 뺌
-        deploy_day = stack.popleft()
-        count = 1
-        
-        while stack and stack[0] <= deploy_day:
-            stack.popleft()
-            count += 1
-        
-        answer.append(count)
-        count = 1
+    # print(time) # 7, 3, 9
     
+    stack.append(time.pop(0))
+    current = stack[0]
+    while time:
+        print(time)
+        if current >= time[0]:
+            stack.append(time.pop(0))
+        else:
+            answer.append(len(stack))
+            current = time[0]
+            stack.clear()
+            # break
+    answer.append(len(stack))
     return answer
-
